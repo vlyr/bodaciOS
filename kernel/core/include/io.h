d13915e4adfe8570fb59345dbe976cfa3e793554
vlyr <vlyr@tuta.io>
2022-08-17 20:25:01 +0300

    inb and outb methods

--- kernel/core/include/io.h
+++ kernel/core/include/io.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-uint8_t inb(uint8_t port);
-void outb(uint8_t port, uint8_t data);
+static inline uint8_t inb(uint16_t port);
+static inline void outb(uint16_t port, uint8_t val);
