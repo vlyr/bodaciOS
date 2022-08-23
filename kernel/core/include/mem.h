405952ea03b56cfd924aac2b21c6d7626de04fd9
vlyr <vlyr@tuta.io>
2022-08-23 19:58:15 +0300

    refactor(kernel/core/mem): start rewriting PMM

--- kernel/core/include/mem.h
+++ kernel/core/include/mem.h
@@ -4,10 +4,10 @@
 #include <stdint.h>
 
 #define PMM_BLOCK_SIZE 4096
-#define BLOCKS_PER_U32 32
+#define PMM_ACCESS_UNIT_LENGTH 8
 
 // Initialize global state for PMM
-void pmm_init(uint64_t base_addr, size_t mem_size);
+void pmm_init(size_t mem_size);
 
 // Initialize a region of `size` pages of memory
 // Returns a pointer to `base_addr`.
@@ -16,5 +16,5 @@ void* pmm_init_region(uint64_t base_addr, size_t size);
 // Deinitialize a region of `size` pages of memory
 void pmm_deinit_region(uint64_t base_addr, size_t size);
 
-// Allocate `n` contiguous page frames and return the starting address
-void* pmm_alloc_frame(size_t n);
+// Allocate a block and return the starting address of it
+void* pmm_alloc_block();
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -14,11 +14,8 @@ void print_multiboot_information(uint64_t* multiboot_information) {
     struct multiboot_tag* t = (struct multiboot_tag*) (multiboot_information + offset);
 
     while (t->type != MULTIBOOT_TAG_TYPE_END) {
-        klog(LOG_MESSAGE_DEBUG, "multiboot tag | size: %d, type: %d\n", t->size, t->type);
-
         switch (t->type) {
         case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO: {
-            pmm_init(0x0100000, ((struct multiboot_tag_basic_meminfo*) t)->mem_upper);
             break;
         }
 
@@ -30,7 +27,10 @@ void print_multiboot_information(uint64_t* multiboot_information) {
                  mmap = (multiboot_memory_map_t*) ((unsigned long) mmap +
                                                    ((struct multiboot_tag_mmap*) t)->entry_size))
                 if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE) {
-                    pmm_init_region(mmap->addr, mmap->len / 1024);
+                    klog(LOG_MESSAGE_DEBUG,
+                         "memory region from multiboot struct: addr = %x, len = %d\n", mmap->addr,
+                         mmap->len);
+                    // pmm_init_region(mmap->addr, mmap->len / 1024);
                 }
 
             break;
@@ -47,7 +47,28 @@ void kmain(uint64_t* multiboot_information) {
 
     klog(LOG_MESSAGE_ERROR, "error message test\n");
     klog(LOG_MESSAGE_WARN, "warning message test\n");
+
+    // Gotta find a more sane way of iterating over the multiboot tags
+    /*struct multiboot_tag* t = (struct multiboot_tag*) (multiboot_information + 8);
+
+    while (1) {
+        if (t->type == MULTIBOOT_TAG_TYPE_BASIC_MEMINFO) {
+            pmm_init(1024 + ((struct multiboot_tag_basic_meminfo*) t)->mem_upper);
+            klog(LOG_MESSAGE_DEBUG, "PMM initialized\n");
+
+            break;
+        }
+
+        t = (struct multiboot_tag*) ((multiboot_uint8_t*) t + ((t->size + 7) & ~7));
+    }*/
+
     print_multiboot_information(multiboot_information);
+
+    /*void* frame = pmm_alloc_block();
+    klog(LOG_MESSAGE_DEBUG, "address returned from `pmm_alloc_frame`: %x\n", frame);
+
+    void* f2 = pmm_alloc_block();
+    klog(LOG_MESSAGE_DEBUG, "address returned from `pmm_alloc_frame`: %x\n", f2);*/
 
     char cmd_buffer[128] = "";
     size_t cmd_buffer_idx;
